99562481629328b1857857318817bf7e31adc2a7
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2024-08-18 08:54:43 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Interlock Landing Page",
+  description: "Interlock Landing Page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="relative">
+      <body className={clsx(dmSans.className, "antialiased")}>
+        {children}
+      </body>
+    </html>
+  );
+}
