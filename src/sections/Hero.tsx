cff1aed286d6863f1ce23627f577faba8e9f824c
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2024-08-20 01:22:38 +0100

    hero-section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,50 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import HeroBackground from "@/assets/hero.png";
+import btnArrow from "@/assets/arrow.png"
+import Hero_Right from "@/assets/hero_right.png"
+
+
 export const Hero = () => {
-  return null;
+  return (
+    <div className="relative flex min-h-[810px] w-full flex-col items-center justify-center overflow-hidden bg-cover px-6 pt-[0px] md:flex-row md:items-start md:px-10 md:pt-[200px] lg:justify-start lg:pt-[200px] xl:px-[159px] 3xl:justify-center ">
+      <div className="absolute inset-0 z-0">
+        <Image 
+          src={HeroBackground} 
+          alt="Hero Background" 
+          layout="fill" 
+          objectFit="cover" 
+          objectPosition="center"
+          priority
+        />
+      </div>
+
+      <div className="relative z-10 flex mx-4 sm:mx-6 md:mx-8 lg:mx-10 xl:mx-auto">
+        <div className="mt-8">
+          <h1 className="w-full max-w-[90%] sm:max-w-[100%] md:max-w-[80%] lg:max-w-[649px] text-[#0E0E0E] text-3xl sm:text-4xl md:text-5xl lg:text-[80px] text-left font-semibold leading-tight sm:leading-tight md:leading-snug lg:leading-[80px] tracking-tight lg:tracking-[-3px]">
+            Empowering Your Finance.
+          </h1>
+    
+          <p className="w-full max-w-[90%] sm:max-w-[100%] md:max-w-[80%] lg:max-w-[523px] text-[#0E0E0E] text-base sm:text-lg md:text-xl lg:text-xl text-left font-medium leading-6 sm:leading-7 md:leading-8 lg:leading-[30px] mt-5 sm:mt-7 mb-8 sm:mb-10 lg:mb-14">
+            Where finance meets technology in seamless harmony. We're dedicated to redefining the future of finance by offering innovative solutions tailored to meet the evolving needs of our clients.
+          </p>
+    
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-10 items-center">
+            <button className="flex items-center justify-center w-full md:w-[202px] lg:w-[202px] sm:w-auto px-6 py-3 sm:py-4 bg-inherit border-2 border-[#131316] text-[#344054] text-center text-base sm:text-lg font-semibold leading-7 rounded-full">
+              Get Started
+            </button>
+      
+            <button className="flex items-center gap-2 sm:gap-3 lg:gap-[10px] text-left text-base sm:text-lg text-[#475467] font-semibold leading-7">
+              Learn More
+              <Image src={btnArrow} alt="Arrow icon" />
+            </button>
+          </div>
+        </div>
+
+      </div>
+      <div>
+          <Image src={Hero_Right} alt="" className="rotate- [-19.51deg] absolute bottom-[-450px] right-0 mx-auto hidden w-[1395px] lg:block 3xl:left-0 3xl:right-[0px]" />
+        </div>
+    </div> 
+  );
 };
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
 
   return (
     <nav>
-      <div className="fixed left-0 right-0 top-[19px] z-50 mx-[24px] flex h-[65px] items-center justify-between rounded-[32px] bg-white/10 px-6 shadow-[0_57px_96px_-8px_rgba(87,92,95,0.25)] outline outline-4 -outline-offset-[4px] outline-white/[38%] backdrop-blur-[4px] md:mx-[40px] md:h-[80px] md:px-8 xl:mx-[95px] 3xl:mx-auto 3xl:max-w-[1250px]">
+      <div className="fixed left-0 right-0 top-[19px] z-50 mx-4 sm:mx-6 md:mx-14 lg:mx-20 xl:mx-auto flex h-20 items-center justify-between rounded-[32px] bg-white/10 px-4 sm:px-6 md:px-8 lg:w-[90%] xl:w-[85%] 2xl:w-[80%] 3xl:max-w-[1249px] shadow-[0_57px_96px_-8px_rgba(87,92,95,0.25)] outline outline-4 -outline-offset-[4px] outline-white/[38%] backdrop-blur-[4px]">
         <div className="flex items-center justify-center gap-10">
           <Image src={Logo} alt="" width={142} height={32} />
           <div className="hidden items-center justify-center gap-[20px] text-base font-semibold leading-[24px] text-[#445568] md:flex md:gap-[32px] xl:text-[18px]">
@@ -35,15 +35,15 @@ export const Navbar = () => {
         </div>
 
         <button
-          className="block md:hidden"
+          className="flex items-center md:hidden"
           onClick={toggleMenu}
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
         >
-          {isMenuOpen ? <FiX width={24} height={24} /> : <FiAlignCenter width={24} height={24} />} 
+          {isMenuOpen ? <FiX width={24} height={24} /> : <FiAlignCenter width={35} height={35} />} 
         </button>
 
         <Link href="/#get-app">
-          <button className="btn-shadow group relative flex h-[38px] w-[110px] items-center justify-center overflow-hidden rounded-full bg-[#131316] text-sm font-medium leading-[24px] text-white outline outline-4 -outline-offset-[0px] outline-white/[55%] transition-all hover:outline-[5px] hover:-outline-offset-1 hover:outline-white/[100%] md:h-[48px] xl:w-[154px] xl:text-[18px]">
+          <button className="btn-shadow group relative flex h-12 w-[154px] items-center justify-center overflow-hidden rounded-full bg-[#131316] text-sm font-medium leading-[24px] text-white outline outline-8 -outline-offset-[0px] outline-white/[55%] transition-all hover:outline-[5px] hover:-outline-offset-1 hover:outline-white/[100%] md:h-[48px] xl:w-[154px] xl:text-[18px]">
             <div className="absolute -bottom-[2px] -left-1 right-0 z-10 !mx-auto h-[0px] w-[160px] rounded-full bg-white transition-all duration-300 group-hover:h-[52px]" />{" "}
             <span className="z-[11] group-hover:text-black transition-all ease-in-out duration-1000">Get App</span>
           </button>
