1afc81a19347f823e2a19f06e59730200eabf09c
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2024-08-27 10:09:35 +0100

    Footer setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { Navbar } from "@/sections/Navbar";
@@ -16,6 +17,7 @@ export default function Home() {
       <ProductShowcase />
       <WhyChooseUs />
       <CallToAction />
+      <Footer />
     </div>
   )
 }
